d85226bbf8640c73c07c61f627a71c12577f3ab5
Anievil <bazilevskiy2002@gmail.com>
2020-06-18 22:22:08 +0300

    create blog block and add video. start work with next blok

---
+++ js/onVideo.js
@@ -0,0 +1,23 @@
+const post = document.querySelector('#videoBlock'),
+video = document.querySelector('#video'),
+playVideo = document.createElement('video'),
+videoBlock = document.querySelector('#videoBlock');
+
+video.addEventListener('click', function(){
+    videoBlock.classList.add('videoPost');
+    playVideo.setAttribute('src', '../video/clip.mp4');
+    playVideo.setAttribute('controls','');
+    playVideo.classList.add('videoPleyer');
+    videoBlock.append(playVideo);
+    check = false;
+})
+
+document.addEventListener('click', (event) => {
+    if(!event.target.closest('.videoPleyer')){ 
+        if(!event.target.closest('.video')){
+            playVideo.classList.remove('videoPleyer');
+            videoBlock.classList.remove('videoPost');
+            playVideo.remove()
+        }
+    }
+});
